c861786517dfb0fe8ab064a5204c2cc9affb43e6
= <=>
2017-02-08 20:52:47 +0100

    added empty classes

---
+++ src/main/java/com/adscoop/mailservice/config/MailConfig.java
@@ -0,0 +1,9 @@
+package com.adscoop.mailservice.config;
+
+/**
+ * Created by thokle on 08/02/2017.
+ */
+public class MailConfig {
+    private String smtp;
+
+}
---
+++ src/main/java/com/adscoop/mailservice/modules/EmailModule.java
@@ -0,0 +1,14 @@
+package com.adscoop.mailservice.modules;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by thokle on 08/02/2017.
+ */
+public class EmailModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
+
