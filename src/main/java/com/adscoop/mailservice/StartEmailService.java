1de6de79fadce38d91c7dce9f0c9fc19bf8b58a8
= <=>
2017-02-08 20:40:12 +0100

    initial commit

---
+++ src/main/java/com/adscoop/mailservice/StartEmailService.java
@@ -0,0 +1,13 @@
+package com.adscoop.mailservice;
+
+/**
+ * Created by thokle on 08/02/2017.
+ */
+public class StartEmailService {
+
+
+    public static void main(String...args){
+
+    }
+
+}
---
+++ src/main/java/com/adscoop/mailservice/entites/CardType.java
@@ -0,0 +1,9 @@
+package com.adscoop.mailservice.entites;
+
+/**
+ * Created by thokle on 22/11/2016.
+ */
+public enum CardType {
+
+    Mastercard, visa;
+}
