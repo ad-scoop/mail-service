eba999a0bb074835b391ec4f679c8dcef5a06596
= <=>
2017-02-12 16:56:16 +0100

    added send mail handler

---
+++ src/main/java/com/adscoop/mailservice/handlers/ActivateUserHandler.java
@@ -0,0 +1,7 @@
+package com.adscoop.mailservice.handlers;
+
+/**
+ * Created by thokle on 11/02/2017.
+ */
+public class ActivateUserHandler {
+}
---
+++ src/main/java/com/adscoop/mailservice/services/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.adscoop.mailservice.services;
+
+/**
+ * Created by thokle on 11/02/2017.
+ */
+public class UserServiceImpl {
+}
